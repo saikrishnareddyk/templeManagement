a2aae03ae4567ca39f4d79ae4ae7dbc0b5edad5b
krishna reddy <kreddy@efficiencyware.com>
2026-05-17 22:46:30 +0530

    db connection & program class changes

--- TempleCsrApi/Program.cs
+++ TempleCsrApi/Program.cs
@@ -1,17 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using TempleCsrApi.Data;
+using TempleCsrApi.Repositories;
+using TempleCsrApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TempleDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IDevoteeRepository, DevoteeRepository>();
+builder.Services.AddScoped<IDevoteeService, DevoteeService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
