d9a30cd8350a309277ff45bfd5bb06110ce21bcd
krishna reddy <kreddy@efficiencyware.com>
2026-05-17 18:53:25 +0530

    devotee model validations

--- TempleCsrApi/Models/Devotee.cs
+++ TempleCsrApi/Models/Devotee.cs
@@ -1,22 +1,27 @@
-﻿namespace TempleCsrApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TempleCsrApi.Models
 {
     public class Devotee
     {
         public int Id { get; set; }
-        
+
+        [Required]
+        [MaxLength(100)]
         public string FullName { get; set; } = string.Empty;
 
+        [Required]
+        [MaxLength(15)]
         public string PhoneNumber { get; set; } = string.Empty;
 
+        [EmailAddress]
         public string? Email { get; set; }
 
+        [MaxLength(50)]
         public string? Gothram { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public DateTime? UpdatedAt { get; set; }
-
-
-
     }
 }
