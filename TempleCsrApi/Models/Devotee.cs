578c857b3467f0a1bea691895b09b4c9d9a9ec40
krishna reddy <kreddy@efficiencyware.com>
2026-05-17 12:01:07 +0530

    devotte model created

---
+++ TempleCsrApi/Models/Devotee.cs
@@ -0,0 +1,22 @@
+﻿namespace TempleCsrApi.Models
+{
+    public class Devotee
+    {
+        public int Id { get; set; }
+        
+        public string FullName { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string? Email { get; set; }
+
+        public string? Gothram { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+
+
+
+    }
+}
